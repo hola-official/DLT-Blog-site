f3c47fe175cdc4de3c82173470e90cad03cb2513
muhammedola <141998553+muhammedola@users.noreply.github.com>
2023-11-07 16:51:51 +0100

    setup package json

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import { useStoreState } from 'easy-peasy';
 
 const Footer = () => {
   const date = new Date();
+  const postCount = useStoreState((state) => state.postCount)
   return (
     <footer className='Footer'>
+      <small>{postCount}</small>
       <p>Copyright &copy; {date.getFullYear()}</p>
     </footer>
   )
